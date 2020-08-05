6f942b6d0e2c4cf4c931dc362a070c54c62a68b7
Orane31 <orane.chaper@gmail.com>
2020-08-05 09:56:44 +0200

    last before redo everything in CRUD

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -13,16 +13,33 @@ class GossipsController < ApplicationController
     # Méthode qui crée un potin vide et l'envoie à une view qui affiche le formulaire pour 'le remplir' (new.html.erb)
   end
 
-  def create
-    @gossip = Gossip.new(title: params[:title], content: params[:content])
     # Méthode qui créé un potin à partir du contenu du formulaire de new.html.erb, soumis par l'utilisateur
+  def create
+    @gossip = Gossip.new(title: params[:title], content: params[:content], user: User.find(21))
+# J'ai créé un user anonymous, son id est le 21, du coup le new gossip aura toutjours le user 21 pour l'instant.
+
     if @gossip.save
+#      @message = "The super potin was succesfully saved !"
+      flash[:success] = "Le livre a été mis à jour."
+      redirect_to index_path
+    else
+      @problem = true 
+      render new_gossip_path
+    end
+    #if @gossip.save
+    #  flash[:success] = "Merci #{@gossip.user.first_name} ! Nous avons pu créer le gossip : #{@gossip.title} "
+    #  redirect_to :controller => 'static_pages', :action => 'index' 
+    #else
+    #  flash[:danger] = "Nous n'avons pas réussi à créer le potin pour la (ou les) raison(s) suivante(s) : #{@gossip.errors.full_messages.each {|message| message}.join('')}"
+    #  render :action => 'new'
     #banner_alert[:success] = "Trop cool!👌 ✅ Ton Gossip a été publié ✅👌" ()
     #redirect_to :controller => 'static_pages', :action => 'index' (basé sur modèle trouvé sur stackoverflow : redirect_to :controller => 'controllername', :action => 'actionname')
-    else
     #banner_alert[:failure] = "❌ Tu n'as pas rempli le formulaire correctement... ❌ 👉 Essaie encore"
     #render :action => 'new'
-    end
+    #end
+
+  end
+
 
   def edit
     # Méthode qui récupère le potin concerné et l'envoie à la view edit (edit.html.erb) pour affichage dans un formulaire d'édition
--- app/models/private_message.rb
+++ app/models/private_message.rb
@@ -2,5 +2,7 @@ class PrivateMessage < ApplicationRecord
   belongs_to :sender, class_name: "User"
   belongs_to :recepient, class_name: "User"
 
-  validates :private_message, presence: true, length: { minimum: 15, maximum: 500 }
+  validates :content,
+  presence: true,
+  length: { minimum: 15, maximum: 500 }
 end
